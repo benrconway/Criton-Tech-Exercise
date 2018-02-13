79692af815b0c2e6e578ad1b12de370a091381ae
benrconway84 <benrconway84@gmail.com>
2018-02-13 22:09:25 +0000

    Refactored for simplicity

--- client/src/app.js
+++ client/src/app.js
@@ -1,20 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import LookUpContainer from './components/look_up_container.jsx';
 
-// I began with the React version found in the components directory, however
-// as I progressed I decided to swap back to vanilla JS and take control of all
-// the aspects of the application logic.
-
-// The program can be run easily through an npm install followed by npm start. 
+// The program can be run easily through an npm install followed by npm start.
 import basicView from './views/basic_view.js';
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  // event.preventDefault()
+
   basicView.displayBasicContent()
 
-  // ReactDOM.render(
-  //   <LookUpContainer/>,
-  //   document.getElementById('app')
-  // )
 });
